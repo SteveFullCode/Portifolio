3dd75970c0b4e9c08ca4bef51bd2719e2bdd60df
SteveDev <Steveherisondev@gmail.com>
2024-03-11 12:19:48 -0300

    adicionado projetos

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,11 @@
 export default function Footer() {
   return (
-    <footer className="h-full py-10 bg-zinc-200 dark:bg-white rounded-t-full">
-      <div className="flex justify-between items-center container">
-        <h2 className="flex gap-2 items-center text-black font-semibold">
+    <footer className="h-full py-3  md:py-10 bg-zinc-200 dark:bg-white rounded-t-full">
+      <div className="md:flex md:justify-between items-center container flex-col md:flex-row">
+        <h2 className=" text-center text-black font-semibold">
           Obrigado, Foi um prazer!
         </h2>
-        <h2 className="font-semibold text-lg">
+        <h2 className="font-semibold text-lg text-center  ">
           <span>SteveDev © 2024 . todos os direitos reservados</span>
         </h2>
       </div>
